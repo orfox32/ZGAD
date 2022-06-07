88e927741086a723d90c26505d7cc7b9969e0da8
Orfox <77915094+orfox32@users.noreply.github.com>
2022-06-07 20:04:12 +0800

    Create lazy-load.js

---
+++ scripts/lazy-load.js
@@ -0,0 +1,24 @@
+const load = document.querySelectorAll('img')
+
+const lazyLoad = loads => {
+    const intobs = new IntersectionObserver((entries, observer) => {
+        
+        entries.forEach(entry => {
+            console.log('❤')
+
+            if(entry.isIntersecting){
+                const img = entry.target;
+                const src = img.getAttribute('data-lazy')
+
+                img.setAttribute('src', src)
+                img.classList.add('fade')
+
+                observer.disconnect()
+            }
+        })
+    })
+
+    intobs.observe(loads)
+}
+
+load.forEach(lazyLoad)
