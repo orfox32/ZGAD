0b8aa8cc9772bd233321b9ca2579812ad0bef64b
Orfox <77915094+orfox32@users.noreply.github.com>
2022-06-14 10:31:37 +0800

    Update owlattr.js

--- scripts/owlattr.js
+++ scripts/owlattr.js
@@ -1,7 +1,7 @@
 $('.owl-carousel').each(function() {
-    //Find each set of dots in this carousel
+   
   $(this).find('.owl-dot').each(function(index) {
-    //Add one to index so it starts from 1
+    
     $(this).attr('aria-label', index + 1);
   });
 });
