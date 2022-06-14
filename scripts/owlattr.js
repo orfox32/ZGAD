521b44f1d41ff9adfcc9139adfc71491a2779d86
Orfox <77915094+orfox32@users.noreply.github.com>
2022-06-14 10:35:15 +0800

    Update owlattr.js

--- scripts/owlattr.js
+++ scripts/owlattr.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 $('.owl-carousel').each(function() {
    
   $(this).find('.owl-dot').each(function(index) {
@@ -5,3 +6,4 @@ $('.owl-carousel').each(function() {
     $(this).attr('aria-label', index + 1);
   });
 });
+})
